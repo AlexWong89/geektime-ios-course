0b27c1c74e4b32bf106ff54c6e4a103a0d1a8fa7
dequanzhu <147280593@qq.com>
2019-05-20 16:14:28 +0800

    UPDATE

--- project/SampleApp/AppDelegate.m
+++ project/SampleApp/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "GTNewsViewController.h"
 #import "GTVideoViewController.h"
 #import "GTRecommendViewController.h"
 
@@ -25,26 +25,22 @@
     
     UITabBarController *tabbarController = [[UITabBarController alloc] init];
     
-    
-    ViewController *viewController = [[ViewController alloc] init];
-    
-//    UIViewController *controller1 = [[UIViewController alloc]init];
-//    controller1.view.backgroundColor = [UIColor redColor];
-    viewController.tabBarItem.title = @"新闻";
-    viewController.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/page@2x.png"];
-    viewController.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/page_selected@2x.png"];
+    GTNewsViewController *newsViewController = [[GTNewsViewController alloc] init];
+    newsViewController.tabBarItem.title = @"新闻";
+    newsViewController.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/page@2x.png"];
+    newsViewController.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/page_selected@2x.png"];
     
     GTVideoViewController *videoController = [[GTVideoViewController alloc]init];
 
     GTRecommendViewController *recommendController = [[GTRecommendViewController alloc]init];
 
-    UIViewController *controller4 = [[UIViewController alloc]init];
-    controller4.view.backgroundColor = [UIColor lightGrayColor];
-    controller4.tabBarItem.title = @"我的";
-    controller4.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/home@2x.png"];
-    controller4.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/home_selected@2x.png"];
+    UIViewController *mineViewController = [[UIViewController alloc]init];
+    mineViewController.view.backgroundColor = [UIColor lightGrayColor];
+    mineViewController.tabBarItem.title = @"我的";
+    mineViewController.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/home@2x.png"];
+    mineViewController.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/home_selected@2x.png"];
 
-    [tabbarController setViewControllers:@[viewController,videoController,recommendController,controller4]];
+    [tabbarController setViewControllers:@[newsViewController,videoController,recommendController,mineViewController]];
     
     tabbarController.delegate = self;
     
